7129a043f32393e55de798048813f22fc7efd53f
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-19 21:09:00 +0900

    LikeButtonを動かしてみる

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
+const LikeButton = () => {
+  const [liked, setliked] = React.useState(false)
+
+  const taggleLiked = () => setliked(!liked)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="likeButton" onClick={taggleLiked}>
+      {liked? "いいね済" : "いいね前"}
+    </button>
+  );
+}
+
+const App = () => {
+  return (
+    <>
+      <LikeButton />
+    </>
   );
 }
 
